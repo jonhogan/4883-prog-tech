44f08de55fdcf32c9d503b9b01f278cd93192f37
Jon <18120090+jonhogan@users.noreply.github.com>
2021-10-28 23:31:42 -0500

    update 3n+1

--- Assignments/3n plus one/100.cpp
+++ Assignments/3n plus one/100.cpp
@@ -1,14 +1,40 @@
-#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int three_n(long long x, long long y){
+    int count = 1;
+    int size;
+    int n;
+
+    if(x <= y){
+        n = x;
+        size = y;
+    }else{
+        n = y;
+        size = x;
+    }
+
+    while( n != 1){
+        if(n%2){
+            n = (3*n)+1;
+        }else{n = n/2;}
+
+        count++;
+    }
+
+    return count;
+}
 
 int main(){
 
-    int *j;
-    int *i;
+    long long i = 0;
+    long long j = 0;
+    int k;
+    
+    while(scanf("%lld %lld", &i, &j) != EOF){
+        k = three_n(i, j);
 
-    while (scanf("%d %d", &i, &j) != EOF){
-        int k = 0;
-        int n = *j;
-
-
+        printf("%lld %lld %d\n", &i, &j, &k);
     }
+
 }
