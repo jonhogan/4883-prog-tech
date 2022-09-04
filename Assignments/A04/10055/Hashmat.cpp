aac6a63b5e3311d8fc232774d30f65b0e6a91105
Jon <jon.hogan83@gmail.com>
2022-09-04 11:50:57 -0500

    Update Hashmat

--- Assignments/A04/10055/Hashmat.cpp
+++ Assignments/A04/10055/Hashmat.cpp
@@ -5,11 +5,29 @@
 */
 
 #include <stdio.h>
+#include <cmath>
+
+typedef long long int ll;
+
+//scanf macro
+#define scan(A,B) scanf("%lld %lld", &A, &B)
+
+//print macro
+#define PRINT(A,B) printf("%lld\n", A + (~B + 1))
+#define PRINT(B,A) printf("%lld\n", B + (~A + 1))
+
+//While Loop
+#define WHILE(A,B) while(scan(A,B) != EOF)
+
+// If statement
+#define IF(A,B) (A > B)?PRINT(A,B):PRINT(B,A)
+
+
 
 int main() {
-    long long B, A, C;
-    
-    while (scanf("%lld %lld", &A, &B) != EOF) {
-      (A > B)?(printf("%lld\n", A + (~B + 1))):(printf("%lld\n", B + (~A + 1)));
+    long long B, A;
+
+    WHILE(A,B){
+      IF(A,B);
     }
 }
